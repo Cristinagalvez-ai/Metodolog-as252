4772043bd7cff2c2d0ef11b0f223de212ec40424
Cristinagalvez-ai <mcristina.galvez@edu.uah.es>
2026-03-18 21:02:06 +0100

    Add files via upload

---
+++ ejerciciosclases/Tests/CatTest3.java
@@ -0,0 +1,32 @@
+package matcomp.e1.ejerciciosclases.cuaderno3;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CatTest3 {
+
+    @Test
+    void greets() {
+        Cat c = new Cat("Misu");
+
+        // Capturar lo que se imprime por pantalla
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        c.greets();
+
+        assertEquals("Meow\n", output.toString());
+    }
+
+    @Test
+    void testToString() {
+        Cat c = new Cat("Misu");
+
+        String expected = "Cat[Mammal[Animal[name=\"Misu\"]]]";
+
+        assertEquals(expected, c.toString());
+    }
+}
