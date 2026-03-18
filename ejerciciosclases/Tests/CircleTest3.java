af0d3db703ea202b4e9850924831a3af13925467
Cristinagalvez-ai <mcristina.galvez@edu.uah.es>
2026-03-18 21:03:02 +0100

    Add files via upload

---
+++ ejerciciosclases/Tests/CircleTest3.java
@@ -0,0 +1,47 @@
+package matcomp.e1.ejerciciosclases.cuaderno3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircleTest3 {
+
+    @Test
+    void getRadius() {
+        Circle c = new Circle(5.0, "red", true);
+
+        assertEquals(5.0, c.getRadius(), 0.0001);
+    }
+
+    @Test
+    void setRadius() {
+        Circle c = new Circle(5.0, "red", true);
+
+        c.setRadius(10.0);
+
+        assertEquals(10.0, c.getRadius(), 0.0001);
+    }
+
+    @Test
+    void getArea() {
+        Circle c = new Circle(2.0, "red", true);
+
+        assertEquals(Math.PI * 4, c.getArea(), 0.0001);
+    }
+
+    @Test
+    void getPerimeter() {
+        Circle c = new Circle(2.0, "red", true);
+
+        assertEquals(2 * Math.PI * 2, c.getPerimeter(), 0.0001);
+    }
+
+    @Test
+    void testToString() {
+        Circle c = new Circle(5.0, "red", true);
+
+        String expected = "Circle[Shape[color=red,filled=true],radius=5.0]";
+
+        assertEquals(expected, c.toString());
+    }
+}
